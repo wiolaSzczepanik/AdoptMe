3a302e60237bf2b7999360e9289580e409fbcdfe
Wiola <wiolaszcz@gmail.com>
2019-10-26 22:40:24 +0200

    Create new component Pet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Havanese"),
+
+    ])
+
+};
+
 const App = () => {
     return React.createElement(
         "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me!")
+        {id: "something-important"},
+        [React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)
+    ]
     );  
   };
 
