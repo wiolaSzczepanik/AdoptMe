51c0cf8663ce191b21ae5a0b269e988a903e8c8f
Wiola <wiolaszcz@gmail.com>
2019-10-27 12:57:17 +0100

    Add new tools to project, update gitignore

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ])
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {id: "something-important"},
-        [React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {name: "Luna", animal: "Dog", breed:"Havanese"}),
-        React.createElement(Pet, {name: "Kacper", animal: "Cat", breed:"Mixed"}),
-        React.createElement(Pet, {name: "Pepper", animal: "Bird", breed:"Cockatiel"})
-    ]
-    ); 
-  };
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, { name: "Kacper", animal: "Cat", breed: "Mixed" }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    })
+  ]);
+};
 
- ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
